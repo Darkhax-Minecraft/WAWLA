8f242d7f12c8b7e548df6f429e0de098ea57c198
Tyler Hancock <darklime@live.ca>
2014-06-17 18:51:19 -0600

    Re-pushing, aparently the code part didn't make it through.

--- src/main/java/net/darkhax/wawla/modules/ModuleEnchantmentBooks.java
+++ src/main/java/net/darkhax/wawla/modules/ModuleEnchantmentBooks.java
@@ -1,5 +1,18 @@
 package net.darkhax.wawla.modules;
 
+import java.util.ArrayList;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraftforge.common.MinecraftForge;
+
 public class ModuleEnchantmentBooks {
 
+    public static ArrayList<Enchantment> blacklist = new ArrayList<Enchantment>();
+    
+    public ModuleEnchantmentBooks(Boolean enabled) {
+        
+        if (enabled)
+            MinecraftForge.EVENT_BUS.register(this);
+    }
+    
 }
