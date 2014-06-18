49e9a0d0120061f385c8fde3982f513e87270008
Tyler Hancock <darklime@live.ca>
2014-06-17 19:23:49 -0600

    Added module for enchantment books.

--- src/main/java/net/darkhax/wawla/modules/ModuleEnchantmentBooks.java
+++ src/main/java/net/darkhax/wawla/modules/ModuleEnchantmentBooks.java
@@ -2,8 +2,13 @@ package net.darkhax.wawla.modules;
 
 import java.util.ArrayList;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.darkhax.wawla.util.Utilities;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.ItemEnchantedBook;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 
 public class ModuleEnchantmentBooks {
 
@@ -15,4 +20,20 @@ public class ModuleEnchantmentBooks {
             MinecraftForge.EVENT_BUS.register(this);
     }
     
+    @SubscribeEvent
+    public void onItemTooltip(ItemTooltipEvent event) {
+        
+        System.out.println("1");
+        if (event.showAdvancedItemTooltips && event.itemStack.getItem() instanceof ItemEnchantedBook) {
+            
+            System.out.println("2");
+            Enchantment ench = Utilities.getEnchantmentsFromStack(event.itemStack, true)[0];
+            
+            if (!blacklist.contains(ench)) {
+                
+                System.out.println("3");
+                Utilities.wrapStringToList(StatCollector.translateToLocal("description." + ench.getName()), 38, false, event.toolTip);
+            }
+        }
+    }
 }
