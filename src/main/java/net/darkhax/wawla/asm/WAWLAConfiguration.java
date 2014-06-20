54a7f0caf2c8f7779c6761e0d157fb200e79b6e5
Alex <alexrusso225@gmail.com>
2014-06-19 22:12:52 -0400

    Adding configuration options for WAWLA CoreMod

---
+++ src/main/java/net/darkhax/wawla/asm/WAWLAConfiguration.java
@@ -0,0 +1,8 @@
+package net.darkhax.wawla.asm;
+
+import net.minecraftforge.common.config.Property;
+
+public class WAWLAConfiguration {
+
+    public static Property TRANSLATION_CLASS_TRANSFORM;
+}
--- src/main/java/net/darkhax/wawla/asm/WAWLALoadingPlugin.java
+++ src/main/java/net/darkhax/wawla/asm/WAWLALoadingPlugin.java
@@ -1,7 +1,9 @@
 package net.darkhax.wawla.asm;
 
+import java.io.File;
 import java.util.Map;
 
+import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 /**
@@ -10,7 +12,12 @@ import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
  *@author Ghostrec35
  */
 public class WAWLALoadingPlugin implements IFMLLoadingPlugin {
-
+    
+    private static final String CATEGORY_CLASS_TRANSFORMATIONS = "Class Transformations";
+    private static final String CATEGORY_CLASS_TRANSFORMATIONS_COMMENT = "This category contains configurable properties for performing certain class transformations. "
+            + "\nPerforming certain class transformations along with other mods that also perform class transformations can sometimes cause problems."
+            + "\nIf you encounter problems, feel free to disable these transformations, but be aware that some features of this mod will be disabled.";
+    
     @Override
     public String[] getASMTransformerClass() {
 
@@ -32,6 +39,15 @@ public class WAWLALoadingPlugin implements IFMLLoadingPlugin {
     @Override
     public void injectData(Map<String, Object> data) {
 
+        Configuration config = new Configuration(new File(
+                (File) data.get("mcLocation"), "config/wawla.cfg"));
+        config.addCustomCategoryComment(CATEGORY_CLASS_TRANSFORMATIONS,
+                CATEGORY_CLASS_TRANSFORMATIONS_COMMENT);
+        config.load();
+        WAWLAConfiguration.TRANSLATION_CLASS_TRANSFORM = config.get(
+                CATEGORY_CLASS_TRANSFORMATIONS,
+                "Perform Translation Hook Injection?", true);
+        config.save();
     }
 
     @Override
