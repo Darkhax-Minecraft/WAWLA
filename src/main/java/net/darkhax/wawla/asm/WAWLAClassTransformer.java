0bcb88e59393302c496d1be106c6c8ab35a356f9
Alex <alexrusso225@gmail.com>
2014-06-19 21:51:08 -0400

    Adding configuration options to WAWLA CoreMod

--- src/main/java/net/darkhax/wawla/asm/WAWLAClassTransformer.java
+++ src/main/java/net/darkhax/wawla/asm/WAWLAClassTransformer.java
@@ -8,46 +8,47 @@ import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.JumpInsnNode;
 import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.LineNumberNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
 
+import cpw.mods.fml.common.FMLLog;
+
 /**
- * A simple Class Transformer that allows for the results of a string translation to be captured.
- * 
- * @author Ghostrec35
+ *A simple Class Transformer for language hooks. 
+ *@author Ghostrec35 
  **/
 
-public class WAWLAClassTransformer implements IClassTransformer {
-
+public class WAWLAClassTransformer implements IClassTransformer 
+{
     @Override
-    public byte[] transform(String name, String transformedName, byte[] classBytes) {
-
-        if (name.equals("net.minecraft.util.StringTranslate")) {
-
+    public byte[] transform(String name, String transformedName, byte[] classBytes)
+    {
+        if((name.equals("net.minecraft.util.StringTranslate") || name.equals("dd")) && WAWLAConfiguration.TRANSLATION_CLASS_TRANSFORM.getBoolean(true))
+        {
             return injectStrTransHook(classBytes);
         }
-
         return classBytes;
     }
 
-    private byte[] injectStrTransHook(byte[] classBytes) {
-
+    private byte[] injectStrTransHook(byte[] classBytes)
+    {
         boolean isInjected = false;
-
+        
         ClassReader cr = new ClassReader(classBytes);
         ClassNode cn = new ClassNode();
         cr.accept(cn, 0);
-
-        for (MethodNode node : cn.methods) {
-
-            if (node.name.equals("tryTranslateKey") && node.desc.equals("(Ljava/lang/String;)Ljava/lang/String;")) {
-
-                for (int i = 0; i < node.instructions.size(); i++) {
-
-                    if (node.instructions.get(i).getOpcode() == Opcodes.IFNONNULL && !isInjected) {
-
+        for(MethodNode node : cn.methods)
+        {
+            if((node.name.equals("tryTranslateKey") || node.name.equals("func_135064_c")) && node.desc.equals("(Ljava/lang/String;)Ljava/lang/String;"))
+            {
+                for(int i = 0; i < node.instructions.size(); i++)
+                {
+                    if(node.instructions.get(i).getOpcode() == Opcodes.IFNONNULL && !isInjected)
+                    {
                         AbstractInsnNode location = node.instructions.get(i - 4);
                         InsnList list = new InsnList();
                         LabelNode l0 = new LabelNode();
@@ -62,7 +63,7 @@ public class WAWLAClassTransformer implements IClassTransformer {
                 }
             }
         }
-
+        
         ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES + ClassWriter.COMPUTE_MAXS);
         cn.accept(writer);
         return writer.toByteArray();
