e0caafc2a2f7ec12db87ad079ec66aab6e165328
Tyler Hancock <darklime@live.ca>
2014-06-25 02:21:39 -0600

    Formatted plugin class. Jar should remain unchanged.

--- src/main/java/net/darkhax/wawla/asm/WAWLALoadingPlugin.java
+++ src/main/java/net/darkhax/wawla/asm/WAWLALoadingPlugin.java
@@ -7,17 +7,15 @@ import net.minecraftforge.common.config.Configuration;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 /**
- *ASM Loading plugin for WAWLA.
- *
- *@author Ghostrec35
+ * ASM Loading plugin for WAWLA.
+ * 
+ * @author Ghostrec35
  */
 public class WAWLALoadingPlugin implements IFMLLoadingPlugin {
-    
+
     private static final String CATEGORY_CLASS_TRANSFORMATIONS = "Class Transformations";
-    private static final String CATEGORY_CLASS_TRANSFORMATIONS_COMMENT = "This category contains configurable properties for performing certain class transformations. "
-            + "\nPerforming certain class transformations along with other mods that also perform class transformations can sometimes cause problems."
-            + "\nIf you encounter problems, feel free to disable these transformations, but be aware that some features of this mod will be disabled.";
-    
+    private static final String CATEGORY_CLASS_TRANSFORMATIONS_COMMENT = "This category contains configurable properties for performing certain class transformations. " + "\nPerforming certain class transformations along with other mods that also perform class transformations can sometimes cause problems." + "\nIf you encounter problems, feel free to disable these transformations, but be aware that some features of this mod will be disabled.";
+
     @Override
     public String[] getASMTransformerClass() {
 
@@ -39,14 +37,10 @@ public class WAWLALoadingPlugin implements IFMLLoadingPlugin {
     @Override
     public void injectData(Map<String, Object> data) {
 
-        Configuration config = new Configuration(new File(
-                (File) data.get("mcLocation"), "config/wawla.cfg"));
-        config.addCustomCategoryComment(CATEGORY_CLASS_TRANSFORMATIONS,
-                CATEGORY_CLASS_TRANSFORMATIONS_COMMENT);
+        Configuration config = new Configuration(new File((File) data.get("mcLocation"), "config/wawla.cfg"));
+        config.addCustomCategoryComment(CATEGORY_CLASS_TRANSFORMATIONS, CATEGORY_CLASS_TRANSFORMATIONS_COMMENT);
         config.load();
-        WAWLAConfiguration.TRANSLATION_CLASS_TRANSFORM = config.get(
-                CATEGORY_CLASS_TRANSFORMATIONS,
-                "Perform Translation Hook Injection?", true);
+        WAWLAConfiguration.TRANSLATION_CLASS_TRANSFORM = config.get(CATEGORY_CLASS_TRANSFORMATIONS, "Perform Translation Hook Injection?", true);
         config.save();
     }
 
