0614f81229849dce2373b16879c8b49c75b49a8a
Tyler Hancock <darklime@live.ca>
2014-06-24 23:23:02 -0600

    Added a date reference.

--- src/main/java/net/darkhax/wawla/util/Reference.java
+++ src/main/java/net/darkhax/wawla/util/Reference.java
@@ -1,5 +1,8 @@
 package net.darkhax.wawla.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,4 +15,5 @@ public class Reference {
     public static final String CLIENT = "net.darkhax.wawla.proxy.ProxyClient";
 
     public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+    public static final DateFormat DATE = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss");
 }
