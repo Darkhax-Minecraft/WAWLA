a4b10bae44283f92f5e0908aa4806d0c111bb565
Tyler Hancock <darklime@live.ca>
2014-06-24 21:53:25 -0600

    Added some javadocs, the strings are now saved to a list that can be accessed or printed.

--- src/main/java/net/darkhax/wawla/handler/TranslationHooks.java
+++ src/main/java/net/darkhax/wawla/handler/TranslationHooks.java
@@ -1,9 +1,47 @@
 package net.darkhax.wawla.handler;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 public class TranslationHooks {
+
+    static ArrayList<String> lines = new ArrayList<String>();
+    static File logFile = new File("logs/missingStrings.txt");
     
     public static void tryTranslateKey(String key, String translation) {
+
+        String output = "Key: " + key + ", Translation: " + translation;
+        if (translation == null && !lines.contains(output))
+            lines.add("Key: " + key + ", Translation: " + translation);
+    }
+    
+    static void print(String line) {
         
-        System.out.println("Hook Working: Key: " + key + ", Translation: " + translation);
+        BufferedWriter writer = null;
+        try {
+
+            writer = new BufferedWriter(new FileWriter(logFile));
+            writer.write(line);
+            writer.newLine();
+        }
+
+        catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        finally {
+
+            try {
+
+                writer.close();
+            }
+
+            catch (Exception e) {
+
+            }
+        }
     }
 }
