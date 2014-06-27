b61c7a7bd6c4445f920c853436bc4753de742fd2
Tyler Hancock <darklime@live.ca>
2014-06-27 04:41:09 -0600

    Added wawla support.

--- src/main/java/net/darkhax/wawla/handler/WailaHandler.java
+++ src/main/java/net/darkhax/wawla/handler/WailaHandler.java
@@ -6,6 +6,7 @@ import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
+import net.darkhax.wawla.modules.Module;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.event.FMLInterModComms;
@@ -21,24 +22,33 @@ public class WailaHandler implements IWailaDataProvider {
     @Override
     public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
 
-        return null;
+        return (accessor.getStack() != null) ? accessor.getStack() : null;
     }
 
     @Override
     public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
 
+        for (Module module : Module.modules)
+            module.onWailaBlockName(itemStack, currenttip, accessor);
+        
         return currenttip;
     }
 
     @Override
     public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
 
+        for (Module module : Module.modules)
+            module.onWailaBlockDescription(itemStack, currenttip, accessor);
+        
         return currenttip;
     }
 
     @Override
     public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
 
+        for (Module module : Module.modules)
+            module.onWailaBlockTail(itemStack, currenttip, accessor);
+        
         return currenttip;
     }
 
