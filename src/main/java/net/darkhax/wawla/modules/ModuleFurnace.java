097dc77a4c4e4b72545d3dfa7794529966473eca
Darkhax <darklime@live.ca>
2014-07-21 09:22:18 -0600

    Did some furnace things.

--- src/main/java/net/darkhax/wawla/modules/ModuleFurnace.java
+++ src/main/java/net/darkhax/wawla/modules/ModuleFurnace.java
@@ -3,8 +3,12 @@ package net.darkhax.wawla.modules;
 import java.util.List;
 
 import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaRegistrar;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntityFurnace;
+import net.minecraft.util.StatCollector;
 
 public class ModuleFurnace extends Module {
 
@@ -13,7 +17,52 @@ public class ModuleFurnace extends Module {
 
         if (access.getTileEntity() != null && access.getTileEntity() instanceof TileEntityFurnace) {
 
-            tooltip.add("Time: " + access.getNBTData().getInteger("BurnTime"));
+            TileEntityFurnace furnace = (TileEntityFurnace) access.getTileEntity();
+            int burnTime = access.getNBTData().getInteger("BurnTime") / 20;
+
+            if (burnTime > 0)
+                tooltip.add(StatCollector.translateToLocal("tooltip.burnTime") + ": " + burnTime + " " + StatCollector.translateToLocal("tooltip.seconds"));
+
+            if (access.getPlayer().isSneaking()) {
+
+                ItemStack[] furnaceStacks = generateStacksForFurnace(access.getNBTData());
+
+                if (furnaceStacks[0] != null) 
+                    tooltip.add(StatCollector.translateToLocal("tooltip.input") + ": " + furnaceStacks[0].getDisplayName() + " X " + furnaceStacks[0].stackSize);
+                
+                if (furnaceStacks[1] != null)
+                    tooltip.add(StatCollector.translateToLocal("tooltip.fuel") + ": " + furnaceStacks[1].getDisplayName() + " X " + furnaceStacks[1].stackSize);
+                
+                if (furnaceStacks[2] != null)
+                    tooltip.add(StatCollector.translateToLocal("tooltip.output") + ": " + furnaceStacks[2].getDisplayName() + " X " + furnaceStacks[2].stackSize);
+            }
         }
+    }
+
+    @Override
+    public void onWailaRegistrar(IWailaRegistrar register) {
+
+        register.registerSyncedNBTKey("*", TileEntityFurnace.class);
+    }
+    
+    /**
+     * This method allows for a list of ItemStacks to be retrieved from a furnace. 
+     * @param tag: NBTTagCompound containing nbt data for the entity. 
+     * @return ItemStack[]: An array of ItemStacks contained within the furnace. 
+     */
+    public ItemStack[] generateStacksForFurnace(NBTTagCompound tag) {
+        
+        ItemStack[] stacks = new ItemStack[3];
+        NBTTagList list = tag.getTagList("Items", 10);
+        for (int i = 0; i < list.tagCount(); ++i) {
+            
+            NBTTagCompound compound = list.getCompoundTagAt(i);
+            byte byt = compound.getByte("Slot");
+
+            if (byt >= 0 && byt < stacks.length)
+                stacks[byt] = ItemStack.loadItemStackFromNBT(compound);
+        }
+        
+        return stacks;
     }
 }
