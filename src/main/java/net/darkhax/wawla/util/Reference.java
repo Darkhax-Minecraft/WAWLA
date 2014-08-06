59ac656ec746ceb70bb2b64fc9f728b8a7c93ba0
Darkhax <darklime@live.ca>
2014-08-05 21:53:15 -0400

    Updated version to 1.0.3 so I don't forget when we push.

--- src/main/java/net/darkhax/wawla/util/Reference.java
+++ src/main/java/net/darkhax/wawla/util/Reference.java
@@ -10,7 +10,7 @@ public class Reference {
     
     public static final String MODID = "wawla";
     public static final String MOD_NAME = "What Are We Looking At";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.3";
     public static final String SERVER = "net.darkhax.wawla.proxy.ProxyCommon";
     public static final String CLIENT = "net.darkhax.wawla.proxy.ProxyClient";
     
