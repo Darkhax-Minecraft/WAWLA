45ced5209875d7d025078d5854abf52aa299888c
Darkhax <darklime@live.ca>
2014-08-09 03:20:16 -0400

    Created a developer module for loose development. Plan to kill it later on.

---
+++ src/main/java/net/darkhax/wawla/modules/ModuleDeveloperMode.java
@@ -0,0 +1,28 @@
+package net.darkhax.wawla.modules;
+
+import java.util.List;
+
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaRegistrar;
+import net.darkhax.wawla.util.Utilities;
+import net.minecraft.item.ItemStack;
+
+public class ModuleDeveloperMode extends Module {
+
+    public ModuleDeveloperMode(boolean enabled) {
+    
+        super(enabled);
+    }
+    
+    public void onWailaBlockDescription (ItemStack stack, List<String> tooltip, IWailaDataAccessor access, IWailaConfigHandler config) {
+        
+        if (access.getTileEntity() != null && config.getConfig("option.wawla.devmode"))
+            Utilities.wrapStringToList(access.getNBTData().toString(), 40, true, tooltip);
+    }
+    
+    public void onWailaRegistrar (IWailaRegistrar register) {
+        
+        register.addConfig("Wawla", "option.wawla.devmode");
+    }
+}
