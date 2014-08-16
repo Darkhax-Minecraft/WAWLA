2351218ecbb0f4d6e7ea8374728d3251e4ef1d7b
Darkhax <darklime@live.ca>
2014-08-15 19:01:59 -0600

    Final changes before release.

---
+++ src/main/java/net/darkhax/wawla/modules/ModuleToolStats.java
@@ -0,0 +1,11 @@
+package net.darkhax.wawla.modules;
+
+public class ModuleToolStats extends Module {
+
+    public ModuleToolStats(boolean enabled) {
+    
+        super(enabled);
+    }  
+    
+    
+}
