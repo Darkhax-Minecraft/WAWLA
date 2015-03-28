99b81b48556a9f100b5600aea8d12e834cc2104b
Darkhax <darklime@live.ca>
2015-03-27 23:04:35 -0600

    Added support for making pixelmon data always show up.

--- src/main/java/net/darkhax/wawla/addons/pixelmon/AddonPixelmonEntities.java
+++ src/main/java/net/darkhax/wawla/addons/pixelmon/AddonPixelmonEntities.java
@@ -8,6 +8,7 @@ import mcp.mobius.waila.api.IWailaEntityProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import net.darkhax.wawla.util.Utilities;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -51,29 +52,29 @@ public class AddonPixelmonEntities implements IWailaEntityProvider {
     @Override
     public List<String> getWailaBody(Entity entity, List<String> tip, IWailaEntityAccessor data, IWailaConfigHandler cfg) {
 
-        if (Utilities.compareByClass(classEntityPixelmon, entity.getClass()) && data.getNBTData() != null) {
-
+        boolean canDisplay = (cfg.getConfig(useSneak)) ? data.getPlayer().isSneaking() : true;
+        
+        if (canDisplay && Utilities.compareByClass(classEntityPixelmon, entity.getClass()) && data.getNBTData() != null) {
+      
             NBTTagCompound tag = data.getNBTData();
-            if (data.getPlayer().isSneaking()) {
 
-                if (natureList != null && cfg.getConfig(showNature))
-                    tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.nature") + ": " + natureList[tag.getShort("Nature")]);
+            if (natureList != null && cfg.getConfig(showNature))
+                tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.nature") + ": " + natureList[tag.getShort("Nature")]);
 
-                if (cfg.getConfig(showAbility)) {
-                    
-                    EnumChatFormatting abilityColor = (tag.getInteger("AbilitySlot") == 2) ? EnumChatFormatting.GOLD : EnumChatFormatting.GRAY;
-                    tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.ability") + ": " + abilityColor + tag.getString("Ability"));
-                }
-
-                if (sizeList != null && cfg.getConfig(showSize))
-                    tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.size") + ": " + sizeList[tag.getShort("Growth")]);
-
-                if (cfg.getConfig(showFriendship))
-                    tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.happiness") + ": " + tag.getInteger("Friendship"));
-
-                if (cfg.getConfig(showHeldItem) && tag.hasKey("HeldItemStack"))
-                    tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.helditem") + ": " + ItemStack.loadItemStackFromNBT((NBTTagCompound) tag.getTag("HeldItemStack")).getDisplayName());
+            if (cfg.getConfig(showAbility)) {
+                
+                EnumChatFormatting abilityColor = (tag.getInteger("AbilitySlot") == 2) ? EnumChatFormatting.GOLD : EnumChatFormatting.GRAY;
+                tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.ability") + ": " + abilityColor + tag.getString("Ability"));
             }
+
+            if (sizeList != null && cfg.getConfig(showSize))
+                tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.size") + ": " + sizeList[tag.getShort("Growth")]);
+
+            if (cfg.getConfig(showFriendship))
+                tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.happiness") + ": " + tag.getInteger("Friendship"));
+
+            if (cfg.getConfig(showHeldItem) && tag.hasKey("HeldItemStack"))
+                tip.add(StatCollector.translateToLocal("tooltip.wawla.pixelmon.helditem") + ": " + ItemStack.loadItemStackFromNBT((NBTTagCompound) tag.getTag("HeldItemStack")).getDisplayName());
         }
 
         return tip;
@@ -98,11 +99,13 @@ public class AddonPixelmonEntities implements IWailaEntityProvider {
 
         AddonPixelmonEntities dataProvider = new AddonPixelmonEntities();
 
+        register.addConfig("Pixelmon", useSneak);
         register.addConfig("Pixelmon", showAbility);
         register.addConfig("Pixelmon", showFriendship);
         register.addConfig("Pixelmon", showHeldItem);
         register.addConfig("Pixelmon", showNature);
         register.addConfig("Pixelmon", showSize);
+        register.addConfig("Pixelmon", showIV);
         register.registerBodyProvider(dataProvider, classEntityPixelmon);
         natureList = Utilities.generateElementArray(enumNature);
         sizeList = Utilities.generateElementArray(enumGrowth);
@@ -112,11 +115,13 @@ public class AddonPixelmonEntities implements IWailaEntityProvider {
     public static Class enumNature = null;
     public static Class enumGrowth = null;
 
+    private static String useSneak = "wawla.pixelmon.useSneak";
     private static String showAbility = "wawla.pixelmon.showAbility";
     private static String showFriendship = "wawla.pixelmon.showFriendship";
     private static String showHeldItem = "wawla.pixelmon.showHeldItem";
     private static String showNature = "wawla.pixelmon.showNature";
     private static String showSize = "wawla.pixelmon.showSize";
+    private static String showIV = "wawla.pixelmon.showIV";
 
     private static String[] natureList = null;
     private static String[] sizeList = null;
