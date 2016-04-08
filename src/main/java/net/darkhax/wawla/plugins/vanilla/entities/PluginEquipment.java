a4afb18bcd029ba18cfe067172f19c131abce3f7
Darkhax <darklime@live.ca>
2016-04-08 10:04:33 -0600

    Added a way to disable required sneaking.

--- src/main/java/net/darkhax/wawla/plugins/vanilla/entities/PluginEquipment.java
+++ src/main/java/net/darkhax/wawla/plugins/vanilla/entities/PluginEquipment.java
@@ -12,15 +12,16 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.common.config.Configuration;
 
-public class PluginEquipment extends InfoProvider{
-
+public class PluginEquipment extends InfoProvider {
+    
     private static boolean enabled = true;
     private static boolean enchantmentStar = true;
+    private static boolean requireSneak = true;
     
     @Override
     public void addEntityInfo (List<String> info, InfoAccess data) {
         
-        if (enabled && data.entity instanceof EntityLivingBase && data.player.isSneaking()) {
+        if (enabled && data.entity instanceof EntityLivingBase && (data.player.isSneaking() || !requireSneak)) {
             
             final EntityLivingBase entity = (EntityLivingBase) data.entity;
             
@@ -29,7 +30,7 @@ public class PluginEquipment extends InfoProvider{
                 final ItemStack stack = entity.getItemStackFromSlot(slot);
                 
                 if (stack != null && stack.getItem() != null)
-                    info.add(I18n.translateToLocal("tooltip.wawla." + slot.getName()) + ": " + stack.getDisplayName() + (enchantmentStar && stack.isItemEnchanted() ? ChatFormatting.LIGHT_PURPLE + I18n.translateToLocal("tooltip.wawla.star"): ""));
+                    info.add(I18n.translateToLocal("tooltip.wawla." + slot.getName()) + ": " + stack.getDisplayName() + (enchantmentStar && stack.isItemEnchanted() ? ChatFormatting.LIGHT_PURPLE + I18n.translateToLocal("tooltip.wawla.star") : ""));
             }
         }
     }
@@ -37,7 +38,8 @@ public class PluginEquipment extends InfoProvider{
     @Override
     public void syncConfig (Configuration config) {
         
-        enabled = config.getBoolean("Equipment", "generic_entities", true, "When enabled, a list of armor that the entity is wearing will be added to the HUD while the player is sneaking.");
+        enabled = config.getBoolean("Equipment", "generic_entities", true, "When enabled, a list of armor that the entity is wearing will be added to the HUD.");
         enchantmentStar = config.getBoolean("Equipment_Star", "generic_entities", true, "When enabled, names of equipment will have a purple star beside them when enchanted.");
+        requireSneak = config.getBoolean("Equipment_Sneak", "generic_entities", true, "When enabled, the player must be sneaking to show information.");
     }
 }
