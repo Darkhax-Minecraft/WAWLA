2d6006f311c84579aed09dbb96fb0c87bb5b2d75
Tyler Hancock <darklime@live.ca>
2016-09-30 14:46:33 -0600

    Gave the version a pump.

--- src/main/java/net/darkhax/wawla/lib/Constants.java
+++ src/main/java/net/darkhax/wawla/lib/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     
     public static final String MODID = "wawla";
     public static final String MOD_NAME = "What Are We Looking At";
-    public static final String VERSION_NUMBER = "2.3.0.0";
+    public static final String VERSION_NUMBER = "2.3.1.0";
     public static final String SERVER = "net.darkhax.wawla.common.CommonProxy";
     public static final String CLIENT = "net.darkhax.wawla.client.ClientProxy";
     public static final Logger LOG = LogManager.getLogger(MOD_NAME);
