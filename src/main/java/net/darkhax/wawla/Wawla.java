67847a3572252be6aa83e6aa891da86c2a70c5e7
Tyler Hancock <darklime@live.ca>
2016-09-30 15:43:55 -0600

    Added temperature support.

--- src/main/java/net/darkhax/wawla/Wawla.java
+++ src/main/java/net/darkhax/wawla/Wawla.java
@@ -23,6 +23,7 @@ import net.darkhax.wawla.plugins.generic.PluginEquipment;
 import net.darkhax.wawla.plugins.generic.PluginHardness;
 import net.darkhax.wawla.plugins.generic.PluginHarvestability;
 import net.darkhax.wawla.plugins.generic.PluginTameable;
+import net.darkhax.wawla.plugins.generic.PluginTemperature;
 import net.darkhax.wawla.plugins.vanilla.PluginEXPOrb;
 import net.darkhax.wawla.plugins.vanilla.PluginFurnace;
 import net.darkhax.wawla.plugins.vanilla.PluginHorse;
@@ -77,6 +78,7 @@ public class Wawla {
         
         // Generic Items
         itemProviders.add(new PluginEnchantmentPower());
+        itemProviders.add(new PluginTemperature());
         if (!Loader.isModLoaded("enchdesc"))
             itemProviders.add(new PluginEnchantmentDescription());
         
