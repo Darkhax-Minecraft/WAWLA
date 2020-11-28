52c5317334cf73d7dbe8efb1a618764cd8060d60
Tyler Hancock <darklime@live.ca>
2020-11-27 19:24:30 -0700

    Delegate feature loading to Hwyla. Makes it more obvious when you forget to install the dependency.

--- src/main/java/net/darkhax/wawla/Wawla.java
+++ src/main/java/net/darkhax/wawla/Wawla.java
@@ -1,51 +1,15 @@
 package net.darkhax.wawla;
 
 import java.text.DecimalFormat;
-import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.darkhax.wawla.lib.Feature;
-import net.darkhax.wawla.plugins.vanilla.FeatureAgeable;
-import net.darkhax.wawla.plugins.vanilla.FeatureArmorPoints;
-import net.darkhax.wawla.plugins.vanilla.FeatureBlastResistance;
-import net.darkhax.wawla.plugins.vanilla.FeatureBreakProgress;
-import net.darkhax.wawla.plugins.vanilla.FeatureEnchantmentPower;
-import net.darkhax.wawla.plugins.vanilla.FeatureHardness;
-import net.darkhax.wawla.plugins.vanilla.FeatureHiddenBlocks;
-import net.darkhax.wawla.plugins.vanilla.FeatureHorse;
-import net.darkhax.wawla.plugins.vanilla.FeatureItemFrame;
-import net.darkhax.wawla.plugins.vanilla.FeaturePlayerHead;
-import net.darkhax.wawla.plugins.vanilla.FeatureVillagerProfession;
-
-import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("wawla")
 public class Wawla {
     
     public static final Logger LOG = LogManager.getLogger("WAWLA");
-    public static final DecimalFormat FORMAT = new DecimalFormat("#.##");   
-    private static final List<Feature> features = NonNullList.create();
-    
-    public Wawla() {
-        
-        features.add(new FeatureAgeable());
-        features.add(new FeatureArmorPoints());
-        features.add(new FeatureBlastResistance());
-        features.add(new FeatureBreakProgress());
-        features.add(new FeatureHardness());
-        features.add(new FeatureHorse());
-        features.add(new FeatureItemFrame());
-        features.add(new FeaturePlayerHead());
-        features.add(new FeatureVillagerProfession());
-        features.add(new FeatureHiddenBlocks());
-        features.add(new FeatureEnchantmentPower());
-    }
-    
-    public static List<Feature> getFeatures() {
-        
-        return features;
-    }
+    public static final DecimalFormat FORMAT = new DecimalFormat("#.##");
 }
--- src/main/java/net/darkhax/wawla/WawlaPlugin.java
+++ src/main/java/net/darkhax/wawla/WawlaPlugin.java
@@ -1,15 +1,44 @@
 package net.darkhax.wawla;
 
+import java.util.List;
+
 import mcp.mobius.waila.api.IRegistrar;
 import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.WailaPlugin;
+import net.darkhax.wawla.lib.Feature;
+import net.darkhax.wawla.plugins.vanilla.FeatureAgeable;
+import net.darkhax.wawla.plugins.vanilla.FeatureArmorPoints;
+import net.darkhax.wawla.plugins.vanilla.FeatureBlastResistance;
+import net.darkhax.wawla.plugins.vanilla.FeatureBreakProgress;
+import net.darkhax.wawla.plugins.vanilla.FeatureEnchantmentPower;
+import net.darkhax.wawla.plugins.vanilla.FeatureHardness;
+import net.darkhax.wawla.plugins.vanilla.FeatureHiddenBlocks;
+import net.darkhax.wawla.plugins.vanilla.FeatureHorse;
+import net.darkhax.wawla.plugins.vanilla.FeatureItemFrame;
+import net.darkhax.wawla.plugins.vanilla.FeaturePlayerHead;
+import net.darkhax.wawla.plugins.vanilla.FeatureVillagerProfession;
+import net.minecraft.util.NonNullList;
 
 @WailaPlugin
 public class WawlaPlugin implements IWailaPlugin {
     
+    private final List<Feature> features = NonNullList.create();
+    
     @Override
     public void register (IRegistrar hwyla) {
         
-        Wawla.getFeatures().forEach(f -> f.initialize(hwyla));
+        this.features.add(new FeatureAgeable());
+        this.features.add(new FeatureArmorPoints());
+        this.features.add(new FeatureBlastResistance());
+        this.features.add(new FeatureBreakProgress());
+        this.features.add(new FeatureHardness());
+        this.features.add(new FeatureHorse());
+        this.features.add(new FeatureItemFrame());
+        this.features.add(new FeaturePlayerHead());
+        this.features.add(new FeatureVillagerProfession());
+        this.features.add(new FeatureHiddenBlocks());
+        this.features.add(new FeatureEnchantmentPower());
+        
+        this.features.forEach(f -> f.initialize(hwyla));
     }
 }
