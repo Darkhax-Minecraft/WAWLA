ed69db4b623f4443fd6c5c952d8dad1781d75d55
Tyler Hancock <darklime@live.ca>
2021-03-29 03:24:46 -0600

    Add a warning message when players forget to install waila.

--- src/main/java/net/darkhax/wawla/Wawla.java
+++ src/main/java/net/darkhax/wawla/Wawla.java
@@ -5,6 +5,7 @@ import java.text.DecimalFormat;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("wawla")
@@ -12,4 +13,12 @@ public class Wawla {
     
     public static final Logger LOG = LogManager.getLogger("WAWLA");
     public static final DecimalFormat FORMAT = new DecimalFormat("#.##");
+    
+    public Wawla() {
+        
+        if (!ModList.get().isLoaded("waila")) {
+            
+            LOG.error("Hwyla is not installed, this is a required mod! Their 1.16.2 file works on 1.16.5! https://www.curseforge.com/minecraft/mc-mods/hwyla/files/all?filter-game-version=2020709689%3A7498");
+        }
+    }
 }
